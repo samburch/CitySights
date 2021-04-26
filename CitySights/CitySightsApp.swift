faeafe07825abc611d9716afb1fd7a01880c76c0
Sam Burch <sam.burch85@gmail.com>
2021-04-26 16:07:05 +0100

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Sam Burch on 26/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
